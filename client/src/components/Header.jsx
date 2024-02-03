b10d8d6c226b672b5008d05dfdbb7b005e1a3647
Sabin Dahal <sabindahal2004@gmail.com>
2024-02-03 22:58:51 +0545

    logo change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to={"/"}>
           <h1 className="font-bold text-md sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Sabin</span>
+            <span className="text-slate-500">Sabin&apos;s</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
